73d92724335a3eabc8aaaa7da63ef42c78b4be2d
YarzarMyoMin <yarzar@atkhayar.com>
2022-03-17 21:54:43 +0630

    GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yn_flutter/controllers/counter_controller.dart';
+import 'package:yn_flutter/pages/second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'YN Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -24,10 +27,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final CounterController c = Get.put(CounterController());
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-    );
+        // Use Obx(()=> to update Text() whenever count is changed.
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+
+        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+        body: Column(
+          children: [
+            ElevatedButton(
+                child: const Text("Go to Other"),
+                onPressed: () {
+                  Get.to(const SecondPage());
+                }),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add), onPressed: c.increment));
   }
 }
